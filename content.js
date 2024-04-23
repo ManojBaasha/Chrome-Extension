6329ce651c96c01894875b65cec343a75484d03a
ManojBaasha <manoj.elango123@gmail.com>
2024-04-23 12:58:21 -0700

    printing cherrypicked classes

--- content.js
+++ content.js
@@ -1,18 +1,31 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.text === "scrapeData") {
-        sendResponse(scrapePage(msg.url));
-    }
+  if (msg.text === "scrapeData") {
+    sendResponse(scrapePage(msg.url));
+  }
 });
 
 function scrapePage(url) {
-    var crs = courses();
-    return crs
+  var crs = courses();
+  return crs;
 }
 
-function courses(){
-    var courses3 = document.querySelectorAll('div.classTitle');
-    var courses4 = document.querySelectorAll('div.meeting, div.clearfix')
-    console.log(courses3)
-    console.log(courses4)
-}
+function courses() {
+  // Select all elements with the class "CourseItem gray-shadow-border clearfix"
+  var courseItems = document.querySelectorAll(
+    ".CourseItem.gray-shadow-border.clearfix",
+  );
 
+  // Iterate over each selected element
+  courseItems.forEach(function (courseItem) {
+    var courseTitle = courseItem.querySelector(".classTitle");
+    console.log(courseTitle.textContent);
+    // Find all elements with the class "meeting clearfix" within the current course item
+    var meetingElements = courseItem.querySelectorAll(".meeting.clearfix");
+
+    // Iterate over each meeting element found within the current course item
+    meetingElements.forEach(function (meetingElement) {
+      // Print the content of the meeting element
+      console.log(meetingElement.textContent);
+    });
+  });
+}
